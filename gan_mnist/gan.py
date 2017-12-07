3f090137a24451291f557d3cac9e47ad00daf52b
Simo Asni <sasni@localhost.localdomain>
2017-12-07 18:58:12 -0500

    cleaned up a bit more and added some comments

--- gan_mnist/gan.py
+++ gan_mnist/gan.py
@@ -3,28 +3,19 @@ import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 
+# Lets load our mnist data
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/")
 
-# Commented out code to visualize the mnist dataset
-
-# sample = mnist.train.next_batch(1)
-# sample_image = sample[0]
-# sample_label = sample[1]
-# print(sample_image.shape, sample_label)
-#
-# sample_image = sample_image.reshape([28, 28])
-# plt.imshow(sample_image, cmap='Greys')
-# plt.show()
-
-# Discriminator network
-
+# Lets define our discriminator model
 def discriminator(images, reuse=False):
+    # This will allow us to call the model more than once have it reuse variables
     if (reuse):
         tf.get_variable_scope().reuse_variables()
 
 
-    # First convolutional and pool layers
+    # First convolutional layers
+    # This finds 32 different 5 x 5 pixel features
     d_w1 = tf.get_variable('d_w1', [5, 5, 1, 32], initializer=tf.truncated_normal_initializer(stddev=0.02))
     d_b1 = tf.get_variable('d_b1', [32], initializer=tf.constant_initializer(0))
     d1 = tf.nn.conv2d(input=images, filter=d_w1, strides=[1, 1, 1, 1], padding='SAME')
@@ -32,7 +23,7 @@ def discriminator(images, reuse=False):
     d1 = tf.nn.relu(d1)
     d1 = tf.nn.avg_pool(d1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-    # Second convolutional and pool layers
+    # Second convolutional layers
     # This fidns 64 different 5 x 5 pixel features
     d_w2 = tf.get_variable('d_w2', [5, 5, 32, 64], initializer=tf.truncated_normal_initializer(stddev=0.02))
     d_b2 = tf.get_variable('d_b2', [64], initializer=tf.constant_initializer(0))
@@ -54,13 +45,14 @@ def discriminator(images, reuse=False):
     d_b4 = tf.get_variable('d_b4', [1], initializer=tf.constant_initializer(0))
     d4 = tf.matmul(d3, d_w4) + d_b4
 
-    # d3 contains unscaled values
     return d4
 
-# Generator network
+# Lets define our generator model
 def generator(z, batch_size, z_dim, reuse=False):
+    # Once more this will allow us to call the model more than once and have it resuse variables
     if (reuse):
         tf.get_variable_scope().reuse_variables()
+
 
     g_w1 = tf.get_variable('g_w1', [z_dim, 3136], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
     g_b1 = tf.get_variable('g_b1', [3136], initializer=tf.truncated_normal_initializer(stddev=0.02))
@@ -69,7 +61,6 @@ def generator(z, batch_size, z_dim, reuse=False):
     g1 = tf.contrib.layers.batch_norm(g1, epsilon=1e-5, scope='bn1')
     g1 = tf.nn.relu(g1)
 
-    # Generate 50 features
     g_w2 = tf.get_variable('g_w2', [3, 3, 1, z_dim/2], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
     g_b2 = tf.get_variable('g_b2', [z_dim/2], initializer=tf.truncated_normal_initializer(stddev=0.02))
     g2 = tf.nn.conv2d(g1, g_w2, strides=[1, 2, 2, 1], padding='SAME')
@@ -78,7 +69,6 @@ def generator(z, batch_size, z_dim, reuse=False):
     g2 = tf.nn.relu(g2)
     g2 = tf.image.resize_images(g2, [56, 56])
 
-    # Generate 25 features
     g_w3 = tf.get_variable('g_w3', [3, 3, z_dim/2, z_dim/4], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
     g_b3 = tf.get_variable('g_b3', [z_dim/4], initializer=tf.truncated_normal_initializer(stddev=0.02))
     g3 = tf.nn.conv2d(g2, g_w3, strides=[1, 2, 2, 1], padding='SAME')
@@ -97,23 +87,7 @@ def generator(z, batch_size, z_dim, reuse=False):
     # Dimensions of g4: batch_size x 28 x 28 x 1
     return g4
 
-# Uncomment to view the generator generate images
-
-batch_size = 100
-z_dimensions = 100
-z_placeholder = tf.placeholder(tf.float32, [None, z_dimensions])
-
-generated_image_output = generator(z_placeholder, 1, z_dimensions)
-z_batch = np.random.normal(0, 1, [1, z_dimensions])
-
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    generated_image = sess.run(generated_image_output,
-                                feed_dict={z_placeholder: z_batch})
-    generated_image = generated_image.reshape([28, 28])
-    plt.imshow(generated_image, cmap='Greys')
-    plt.show()
-
+# reset tensorflow's default graph and lets start training this thing!
 tf.reset_default_graph()
 batch_size =100
 z_dimension = 100
@@ -121,6 +95,7 @@ z_dimension = 100
 z_placeholder = tf.placeholder(tf.float32,[None,z_dimension],name='z_placeholder')
 x_placeholder = tf.placeholder(tf.float32,[None,28,28,1],name='x_placeholder')
 
+# Create a child scope in order to allow us to call the discriminator twice
 with tf.variable_scope(tf.get_variable_scope()):
     Gz = generator(z_placeholder, batch_size, z_dimension)
     # Gz holds the generated images
@@ -131,6 +106,7 @@ with tf.variable_scope(tf.get_variable_scope()):
 
     Dg = discriminator(Gz, reuse=True)
 
+# Get our loss for real and fake discriminator and generator
 d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(Dx),logits=Dx))
 d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.zeros_like(Dg),logits=Dg))
 g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(Dg),logits=Dg))
@@ -142,6 +118,7 @@ g_vars = [var for var in tvars if 'g_' in var.name]
 print ([v.name for v in d_vars])
 print ([v.name for v in g_vars])
 
+# Apply adam optimization based on the loss we got previously
 with tf.variable_scope(tf.get_variable_scope()):
     # Train the discriminator
     d_trainer_fake = tf.train.AdamOptimizer(0.0003).minimize(d_loss_fake, var_list=d_vars)
@@ -150,13 +127,16 @@ with tf.variable_scope(tf.get_variable_scope()):
     # Train the generator
     g_trainer = tf.train.AdamOptimizer(0.0001).minimize(g_loss, var_list=g_vars)
 
+# Define graphs that we will generate for Tensorboard
 tf.summary.scalar('Discriminator_loss_real',d_loss_real)
 tf.summary.scalar('Discriminator_loss_fake',d_loss_fake)
 tf.summary.scalar('Generator_loss',g_loss)
 
+# Lets start our session!
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
+# Tensorboard stuff
 with tf.variable_scope(tf.get_variable_scope()) as scope:
     images_for_tensorboard = generator(z_placeholder, batch_size, z_dimension,reuse=True)
     tf.summary.image('Generated_images', images_for_tensorboard, 5)
@@ -164,16 +144,17 @@ with tf.variable_scope(tf.get_variable_scope()) as scope:
     logdir = 'Tensorboard/' + datetime.datetime.now().strftime('%Y%m%d-%H%M%S') + '/'
     writer = tf.summary.FileWriter(logdir,sess.graph)
 
-#Pre-train discriminator
+#Pre-train discriminator for 3000 iterations - this allows for better training overall
 for i in range(3000):
     z_batch = np.random.normal(0, 1, size=[batch_size,z_dimension])
     real_image_batch = mnist.train.next_batch(batch_size)[0].reshape([batch_size,28,28,1])
     _,__,dLossReal,dLossFake = sess.run([d_trainer_real,d_trainer_fake,d_loss_real,d_loss_fake],
                                        feed_dict={x_placeholder:real_image_batch,z_placeholder:z_batch})
+    # Print our loss for real and fake after every 100 iterations
     if(i%100==0):
         print ('dLossReal: ',dLossReal,'dLossFake: ',dLossFake)
 
-#Train discriminator and generator together
+#Train discriminator and generator together for 100,000 iterations - This part will take a while, so maybe go grab a coffee?
 for i in range(100000):
     real_image_batch = mnist.train.next_batch(batch_size)[0].reshape([batch_size,28,28,1])
     z_batch = np.random.normal(0,1,size=[batch_size,z_dimension])
